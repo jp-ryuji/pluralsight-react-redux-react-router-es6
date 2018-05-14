356d945a8fa8d0ed75c3df0da9fbce23731bd6a0
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-14 22:24:15 +0900

    Update entry point

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-/* eslint-disable no-console */
-console.log('hi');
+import 'babel-polyfill'; // NOTE: There're some features Babel can't transpile, so babel-polyfill is needed here. But it's around 50KB, so it's a significant amount of code.
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css'; // NOTE: Webpack can import css files, too.
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
