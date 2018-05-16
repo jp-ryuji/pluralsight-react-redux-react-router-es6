778792851b5f3a4be273413b09a168db766d68fc
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-05-16 21:58:03 +0900

    Error Handling

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -41,7 +41,11 @@ export class ManageCoursePage extends React.Component {
     event.preventDefault();
     this.setState({ saving: true });
     this.props.actions.saveCourse(this.state.course)
-      .then(() => this.redirect());
+      .then(() => this.redirect())
+      .catch(error => {
+        toastr.error(error);
+        this.setState({ saving: false });
+      });
   }
 
   redirect() {
